827f069aeb64664057dc2833ba9ae61644e4eee3
madhuracodes <madhura84@gmail.com>
2024-07-02 12:57:55 +0200

    added JS functions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
+function refreshWeather(response) {
+  console.log(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  //   console.log("calling searchCity");
+  let apiKey = "40912tb49034f3e73f11616ed1a9396o";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios
+    .get(apiUrl)
+    .then(function (response) {
+      refreshWeather(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
